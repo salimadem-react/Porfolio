e150326e57b3b89c24313bdb1dd7e1adca24ef3d
Salim Salim <salimademm@gmail.com>
2022-12-06 19:17:54 +0200

    navbar setup

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,31 @@
 import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
 export default function Topbar() {
-  return <div className="topbar"></div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            cozycode.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+359885136866</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>salimademm@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
