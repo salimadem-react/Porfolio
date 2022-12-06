dbcdd61fbe61f27f9926b8eb5c98630fcce8430e
Salim Salim <salimademm@gmail.com>
2022-12-06 21:16:42 +0200

    menu setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/topbar/Topbar";
 import Works from "./components/works/Works";
+import Menu from "./components/menu/Menu";
 import "./app.scss";
+import { useState } from "react";
 
 export default function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
